43edcda5294c47a633da6a0032769a64fc0259ba
xie186 <xie186@purdue.edu>
2017-12-06 21:08:30 +0000

    Update

---
+++ lib/test.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+
+import glob
+def insensitive_glob(pattern):
+    def either(c):
+        return '[%s%s]'%(c.lower(),c.upper()) if c.isalpha() else c
+    return glob.glob(''.join(map(either,pattern)))
