0ec397997c54ed9dbb63cd70cbe2af4cffa4d44c
xie186 <xie186@purdue.edu>
2017-12-07 02:57:36 +0000

    Update lib/test.py

--- lib/test.py
+++ lib/test.py
@@ -6,6 +6,13 @@ print(Style.RESET_ALL)
 print('back to normal now')
 
 
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
 import glob
 def insensitive_glob(pattern):
     def either(c):
